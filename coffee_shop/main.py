c6c91c8ef9e8818cb083d4dcf48779367f67c918
Nihal-Reddy-Nagidi <nihal.reddy-i@aidenai.com>
2026-05-12 17:07:07 +0530

    https://fruit-mocha-psi.vercel.app/

--- coffee_shop/main.py
+++ coffee_shop/main.py
@@ -9,11 +9,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174",
-        "http://127.0.0.1:5175",
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://localhost:5175",
+        "https://fruit-mocha-psi.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
